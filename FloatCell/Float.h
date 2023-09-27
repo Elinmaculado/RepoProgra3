111a666bebedbf7e8ab77f4f4c8ffee9d88676e8
Miguel239g <127420871+Elinmaculado@users.noreply.github.com>
2023-09-27 12:02:20 -0600

    FloatCell

---
+++ FloatCell/Float.h
@@ -0,0 +1,42 @@
+#ifndef TEMA1_FLOAT_H
+#define TEMA1_FLOAT_H
+
+class FloatCell {
+public:
+    //Constructor implícito
+    explicit FloatCell(float newvalue = 0);
+
+    //constructor explicito por copia
+    FloatCell(const FloatCell &rhs);
+
+    //constructor explicito por referencia
+    FloatCell(FloatCell &&rhs) noexcept;
+
+    //Destructor, se activa cuando se usa free
+    ~FloatCell() = default;
+
+    //Asignación por copia, para r-valores
+    FloatCell &operator = (const FloatCell &rhs);
+    FloatCell &operator = (FloatCell &&rhs) noexcept;
+
+    FloatCell &operator + (const FloatCell &rhs);
+
+    //Se le dice con que tipo de datos va a trabajar en este caso "float"
+    //Porque sin esto solo puede trabajar con datos de su propia clase
+    FloatCell &operator=(float rhs);
+
+
+    //Función para ver que valor está asignado
+    float getValue() const;
+
+    //funcion para asignar valor
+    //Es void porque solo recibe un valor pero no necesitar regresar nada
+    void setValue(float newValue);
+
+protected:
+    //Variable que utilizamos para que se puedan asignar los valores
+    float storedValue;
+};
+
+
+#endif //TEMA1_FLOAT_H
---
+++ FloatCell/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Float.h"
+
+int main() {
+    FloatCell Cell;
+    float n;
+    std::cout << "intrduzca un numero" << std::endl;
+    std::cin >> n;
+
+    Cell.setValue(n);
+
+    std::cout << "el valor es: " << Cell.getValue() << std::endl;
+
+    Cell = Cell + Cell;
+
+    std::cout << "suma = " << Cell.getValue() << std::endl;
+
+    std::cout << "Tu clase si funciona :D" << std::endl;
+
+}
